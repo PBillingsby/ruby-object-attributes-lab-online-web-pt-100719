76f8c97e01fea1bd6573287d97c5923621232f11
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-13 15:44:08 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed 
+    @breed
+  end
+end
